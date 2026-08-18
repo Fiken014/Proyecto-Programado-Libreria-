676efc99cbfe3be1dbfc76805918b3b7483c53b1
Kendall Bonilla Miranda <154045290+Fiken014@users.noreply.github.com>
2026-08-18 17:19:22 -0600

    Update assembly title to include Randy Artavia

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante lo siguiente
 // conjunto de atributos. Cambie los valores de estos atributos para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("PAV_PF_KendallBonilla")]
+[assembly: AssemblyTitle("PAV_PF_KendallBonilla_RandyArtavia")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
